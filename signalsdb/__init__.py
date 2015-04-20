ef9268e1f45a361cedf93a410b5667c2372b2757
Eugene Eeo <packwolf58@gmail.com>
2015-04-20 08:55:54 +0800

    add meta stuff

---
+++ signalsdb/__init__.py
@@ -0,0 +1,12 @@
+"""
+    signalsdb
+    ~~~~~~~~~
+
+    Python library providing a simple way to query a
+    curated database of Unix signals.
+
+    :copyright: (c) 2015 Eeo Jun
+    :license: MIT, see LICENSE for details.
+"""
+
+__version__ = '0.1'
